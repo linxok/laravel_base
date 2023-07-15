b337c1c014427106edb580d05eea0821c5fbdf56
linxok <linxok@gmail.com>
2023-07-15 16:24:27 +0300

    probe

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,38 @@
-<?phpuse Illuminate\Support\Facades\Route;/*|--------------------------------------------------------------------------| Web Routes|--------------------------------------------------------------------------|| Here is where you can register web routes for your application. These| routes are loaded by the RouteServiceProvider within a group which| contains the "web" middleware group. Now create something great!|*/Route::get('/', function () {    return view('welcome');});Route::group(['namespace'=>'Post'], function (){    Route::get('/posts', 'IndexController')->name('post.index');    Route::get('/posts/create', 'CreateController')->name('post.create');    Route::post('/posts', 'StoreController')->name('post.store');    Route::get('/posts/{post}', 'ShowController')->name('post.show');    Route::get('/posts/{post}/edit', 'EditController')->name('post.edit');    Route::patch('/posts/{post}', 'UpdateController')->name('post.update');    Route::delete('/posts/{post}', 'DestroyController')->name('post.delete');});Route::get('/posts/update', 'PostController@update');Route::get('/posts/delete', 'PostController@delete');Route::get('/main', 'MainController@index')->name('main.index');Route::get('/contact', 'ContactController@index')->name('contact.index');Route::get('/about', 'AboutController@index')->name('about.index');
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['namespace'=>'Post'], function (){
+
+    Route::get('/posts', 'IndexController')->name('post.index');
+    Route::get('/posts/create', 'CreateController')->name('post.create');
+
+    Route::post('/posts', 'StoreController')->name('post.store');
+    Route::get('/posts/{post}', 'ShowController')->name('post.show');
+    Route::get('/posts/{post}/edit', 'EditController')->name('post.edit');
+    Route::patch('/posts/{post}', 'UpdateController')->name('post.update');
+    Route::delete('/posts/{post}', 'DestroyController')->name('post.delete');
+
+});
+
+Route::get('/posts/update', 'PostController@update');
+Route::get('/posts/delete', 'PostController@delete');
+
+Route::get('/main', 'MainController@index')->name('main.index');
+Route::get('/contact', 'ContactController@index')->name('contact.index');
+Route::get('/about', 'AboutController@index')->name('about.index');
